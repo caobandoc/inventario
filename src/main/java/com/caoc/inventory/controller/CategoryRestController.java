099d5d952ef2b9e0fb09a26ef226cad3ecc7b65d
caobandoc <obandocarlosandres@gmail.com>
2023-01-10 19:51:01 -0500

    crear excel de categorias

--- src/main/java/com/caoc/inventory/controller/CategoryRestController.java
+++ src/main/java/com/caoc/inventory/controller/CategoryRestController.java
@@ -3,10 +3,14 @@ package com.caoc.inventory.controller;
 import com.caoc.inventory.model.documents.Category;
 import com.caoc.inventory.model.response.CategoryResponseRest;
 import com.caoc.inventory.services.ICategoryService;
+import com.caoc.inventory.util.CategoryExcelExporter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -33,5 +37,19 @@ public class CategoryRestController {
     @DeleteMapping("/{id}")
     public Mono<ResponseEntity<CategoryResponseRest>> deleteCategory(@PathVariable String id) {
         return categoryService.delete(id);
+    }
+
+    @GetMapping("/export/excel")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=result_categories.xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        Mono<ResponseEntity<CategoryResponseRest>> search = categoryService.search();
+
+        CategoryExcelExporter excelExporter = new CategoryExcelExporter(search.block().getBody().getCategoryResponse().getCategoryList());
+
+        excelExporter.export(response);
     }
 }
